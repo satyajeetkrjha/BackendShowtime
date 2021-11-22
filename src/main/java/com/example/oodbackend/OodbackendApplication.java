dd69d0f2803024a9dfebad126574f176b431cef4
satyajeet <satyajeet@satyajeets-MacB.lan>
2021-11-22 11:13:06 -0500

    magical line in main code

--- src/main/java/com/example/oodbackend/OodbackendApplication.java
+++ src/main/java/com/example/oodbackend/OodbackendApplication.java
@@ -2,8 +2,10 @@ package com.example.oodbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+// this is God level fix so you respect this file Satya
+@SpringBootApplication (exclude = { SecurityAutoConfiguration.class })
 public class OodbackendApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/example/oodbackend/repository/UserRepository.java
@@ -0,0 +1,3 @@
+package com.example.oodbackend.repository;
+public interface UserRepository {
+}
